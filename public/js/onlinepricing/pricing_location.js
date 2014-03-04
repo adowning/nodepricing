bf014e5710e6b04c17a143f715da56ad6f4c5f40
ash <ash@ash-Aspire-5534.(none)>
2014-03-04 14:44:27 -0600

    asdlfkjsdf

--- public/js/onlinepricing/pricing_location.js
+++ public/js/onlinepricing/pricing_location.js
@@ -34,7 +34,7 @@ define(['pricing_data', 'pricing_script', 'build_pricing'], function(prd, ps, bp
 
         var tcprice;
 
-        if (distance.length != 'notassigned') {
+        if (distance != 'notassigned') {
             console.log('zipcode found 2')
             $.each(sortedtripcharges, function(index, element) {
                 if (distance >= element.distance) {
