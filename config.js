1d771d082c97fc7b343ffdff7352beb930e7b53e
ash <ash@ash-Aspire-5534.(none)>
2014-04-17 16:19:01 -0500

    using ashedowning as email mandrill

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // This is just a sample.  This should be in your .gitignore file as it may contain sensitive information (password, secret keys, etc.).
 module.exports = {
     "development": {
-        MANDRILL_USERNAME: 'ashdowning@gmail.com',
-        MANDRILL_API_KEY: 'SvG6_1XZpKq6393pZRZUhg'
+        MANDRILL_USERNAME: 'ashedowning@hotmail.com',
+        MANDRILL_API_KEY: 'BZps4vd0VEtIHmmMQZso2w'
     },
     "production": {
         // Set these env variables in your hosted environment
         // MANDRILL_USERNAME : process.env.MANDRILL_USERNAME,
         // MANDRILL_API_KEY : process.env.MANDRILL_API_KEY
-        MANDRILL_USERNAME: 'ashdowning@gmail.com',
-        MANDRILL_API_KEY: 'SvG6_1XZpKq6393pZRZUhg'
+        MANDRILL_USERNAME: 'ashedowning@hotmail.com',
+        MANDRILL_API_KEY: 'BZps4vd0VEtIHmmMQZso2w'
     }
 }
